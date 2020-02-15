0dd81e7e7ecc43f619b73c86a4117e9f86cfa3e7
Birdgeek <birdgeekthree@gmail.com>
2020-02-14 22:30:44 -0700

    Friends

--- pi-setup.sh
+++ pi-setup.sh
@@ -1,23 +1,24 @@
 #!/bin/bash/
 
 echo "Updating System"
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrades -y
+apt-get update
+apt-get upgrade
+apt-get dist-upgrades
 
 echo "Installing Webmin Pre-reqs"
 
-sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 
 echo "Downloading & Installing Webmin"
 
 wget http://prdownloads.sourceforge.net/webadmin/webmin_1.941_all.deb
-sudo dpkg --install webmin_1.941_all.deb
+
+dpkg --install webmin_1.941_all.deb
 
 echo "Opening Webmin ports in UFW"
 
-sudo ufw allow 10000
-sudo ufw status
+ufw allow 10000
+ufw status
 
 echo "Configuration complete, restarting"
 
